8575728f1ee397b521b2c075f8ace03526c0ec99
Rocio Nerea Martinez Subidia <rociomartinezsubidia@gmail.com>
2025-09-21 07:57:27 -0300

    a

--- static/scripts/generarPuntos.js
+++ static/scripts/generarPuntos.js
@@ -1,9 +1,25 @@
-const numeroAleatorio1 = document.getElementById('numeroAleatorio1')
-const numeroAleatorio2 = document.getElementById('numeroAleatorio2')
 
-const numero1 = Math.floor(Math.random() * 5);
+document.addEventListener('DOMContentLoaded', (event) => {
+    actualizarNumeros();
 
-const numero2 = Math.floor(Math.random() * 5);
+    // Ahora puedes acceder a los elementos del DOM, como el botón
+    const boton = document.getElementById('boton_reiniciar');
+    if (boton) {
+        // También puedes poner aquí el listener del botón
+        boton.addEventListener('click', () => {
+            actualizarNumeros();
+        });
+    }
+});
 
-numeroAleatorio1.textContent = numero1;
-numeroAleatorio2.textContent = numero2;
+function actualizarNumeros() {
+    const numeroAleatorio1 = document.getElementById('numeroAleatorio1');
+    const numeroAleatorio2 = document.getElementById('numeroAleatorio2');
+
+    const numero1 = Math.floor(Math.random() * 5);
+
+    const numero2 = Math.floor(Math.random() * 5);
+
+    numeroAleatorio1.textContent = numero1;
+    numeroAleatorio2.textContent = numero2;
+}
