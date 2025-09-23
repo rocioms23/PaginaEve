4b8033f25f841fe5066e1338cc0924b04bd3bffe
Rocio Nerea Martinez Subidia <rociomartinezsubidia@gmail.com>
2025-09-23 16:48:15 -0300

    Prueba

--- static/scripts/generarPuntos.js
+++ static/scripts/generarPuntos.js
@@ -1,6 +1,6 @@
 
+
 document.addEventListener('DOMContentLoaded', (event) => {
-    actualizarNumeros();
 
     // Ahora puedes acceder a los elementos del DOM, como el botón
     const boton = document.getElementById('boton_reiniciar');
@@ -18,8 +18,31 @@ function actualizarNumeros() {
 
     const numero1 = Math.floor(Math.random() * 5);
 
-    const numero2 = Math.floor(Math.random() * 5);
+    numero2 = Math.floor(Math.random() * 5);
 
+    while (numero1 == numero2) {
+        numero2 = Math.floor(Math.random() * 5);
+    }
+    ganador=0
+    if (numero1 > numero2) {
+        ganador = 1
+    }
+    else {
+        ganador = 2
+    }
     numeroAleatorio1.textContent = numero1;
     numeroAleatorio2.textContent = numero2;
+
+    imagenJugador1 = document.getElementById('jugador1')
+    imagenJugador2 = document.getElementById('jugador2')
+
+    if (ganador == 1) {
+        imagenJugador1.src = "./static/assets/Victoria_2.gif";
+        imagenJugador2.src = "./static/assets/derrota.gif";
+    }
+
+    if(ganador == 2) {
+        imagenJugador1.src = "./static/assets/derrota_2.gif";
+        imagenJugador2.src = "./static/assets/Victoria.gif";
+    }
 }
